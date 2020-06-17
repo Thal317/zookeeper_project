b587316a861cd51f448ac524eb657395182fd1ba
Thal317 <cmgrinstead@gmail.com>
2020-06-17 15:20:28 -0700

    made it prettier

--- zookeeper1.py
+++ zookeeper1.py
@@ -1,4 +1,8 @@
-"Hello world!"
+print("Hello world!")
+input("Press Enter to continue.")
+input("Press Enter to continue..")
+input("Press Enter harder to continue...")
+def make_fart_pizza():
+    print("i made a change!")
 
 make_fart_pizza()
-print("i made a change!")
